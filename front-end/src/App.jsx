fef78471617f417489702fc01be37010416af245
DDG000 <dilshanguruge99930@gmail.com>
2024-05-14 16:31:23 +0530

    Done1

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AOS from 'aos'
-import 'aos/dist/aos.js'
-import 'aos/dist/aos.css'
 import Navigate from './Dilshan/Navigate'
+
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  
   return (
-  <div className='container'>
+  <div>
 
 
-    <Navigate></Navigate>
+    <Navigate />
 
   </div>
   )
--- front-end/src/Dilshan/Navigate.jsx
+++ front-end/src/Dilshan/Navigate.jsx
@@ -1,40 +1,90 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Navigate.css";
+import img1 from '../assets/img2.jpg';
+import img2 from '../assets/img3.jpeg';
+import img3 from '../assets/img4.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export default function Navigate() {
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  useEffect(() => {
+    AOS.init({
+      duration: 3000
+    });
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
-      <div className="container-fluid ">
+    <div>
+      <div className="topnav w-auto" id="myTopnav" style={{ margin: 0 }} >
+        <a href="#home" className="active">Home</a>
+        <a href="#news" className='xx'>Contact us</a>
+        <a href="#contact" className='xx'>Tourisum News</a>
+        <a href="#about" className='xx'>Video Streaming</a>
+        <a href="#about" className='xx'>About us</a>
+        <a href="#about" className='xx'>Upcoming Events</a>
+        <a href="#about" className='xx'>Tourisum Hotline</a>
+        <a href="#about" className='xx'>Sign up</a>
+        <a href="#about" className='xx'>Sign IN</a>
+        {/* Use an arrow function or pass myFunction as a reference */}
+        <a href="javascript:void(0);" className="icon" onClick={() => myFunction()}>
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white", padding:"5rem"}}> Home</a>
-
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> contact us</a>
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> Tourism News</a>
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> Video Streaming</a>
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> About us</a>
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> Tourism Hotline</a>
-            </li>
-            <li className="nav-item">
-            <a className="navbar-brand" href="#" style={{ color: "white" }}> Select Language</a>
-            </li>
-          </ul>
+      <br />
+      <div className="bg-black d-flex justify-content-center align-items-center" style={{ height: 'auto' }} data-aos="zoom-in-down">
+        <div id="carouselExampleIndicators" className="carousel slide w-100" data-ride="carousel" data-interval="2000">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src={img1} alt="First slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={img2} alt="Second slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={img3} alt="Third slide" />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
         </div>
       </div>
-    </nav>
+
+
+      <div className="bg-black d-flex justify-content-center align-items-center" style={{ minHeight: '10vh' }}>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <form className="input-group">
+                <input type="text" className="form-control" style={{ maxWidth: '500px' }} placeholder="Search" aria-label="search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
   );
 }
